ed30c07aed64e8ddc8c5a498779fc1c9ab2d2516
Dmitry <dmitry.olefir@novidea.com>
2024-03-13 09:25:41 +0200

    hw29

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+// App.js
+import React, { Component } from "react";
+import Counter from "./Counter";
+import "./styles.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Counter App</h1>
+        <Counter />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Counter.js
@@ -0,0 +1,45 @@
+// Counter.js
+import React, { Component } from "react";
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  increment = () => {
+    this.setState((prevState) => ({
+      count: prevState.count + 1,
+    }));
+  };
+
+  decrement = () => {
+    this.setState((prevState) => ({
+      count: prevState.count - 1,
+    }));
+  };
+
+  reset = () => {
+    this.setState({
+      count: 0,
+    });
+  };
+
+  render() {
+    return (
+      <div className="counter">
+        <button onClick={this.decrement}>-</button>
+        <span>{this.state.count}</span>
+        <button onClick={this.increment}>+</button>
+        <br />
+        <button className="reset-button" onClick={this.reset}>
+          Reset
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
